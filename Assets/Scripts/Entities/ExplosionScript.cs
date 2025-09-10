189455ab5fe9bb53bd8748782fab08f69974f193
seesee7164 <checkeredsage54@gmail.com>
2025-09-10 00:08:24 -0400

    Updating Instantiations
    Updating rocks and explosions effects to use a queue rather than instantiate themselves

---
+++ Assets/Scripts/Entities/ExplosionScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+public class ExplosionScript : MonoBehaviour
+{
+    [Header("Component")]
+    public SpriteRenderer sprite;
+    public Animator animator;
+    public LaserScript laserScript;
+
+    [Header("variable")]
+    public int index;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+    public void restartExplosion()
+    {
+        animator.Play("Laser Explosion");
+        StartCoroutine(HandleDeath());
+    }
+    IEnumerator HandleDeath()
+    {
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+        animator.SetBool("hasDied", false);
+        if (laserScript != null)
+        {
+            laserScript.ExplosionKilled(index);
+        }
+        gameObject.SetActive(false);
+    }
+}
