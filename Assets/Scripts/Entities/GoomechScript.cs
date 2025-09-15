bc4614d5ca6fe0db0e8c0d6ce6b238e5d75867e5
seesee7164 <checkeredsage54@gmail.com>
2025-09-14 21:39:25 -0400

    updating knockback and adding spikes
    updated how knockback works for spikes and bosses so its stronger and added spikes

--- Assets/Scripts/Entities/GoomechScript.cs
+++ Assets/Scripts/Entities/GoomechScript.cs
@@ -26,6 +26,7 @@ public class GoomechScript : MonoBehaviour
     public Rigidbody2D myRigidBody2D;
     public SpriteRenderer sprite;
     public LayerMask groundLayer;
+    public LayerMask spikeLayer;
     public LayerMask enemyLayer;
     public BoxCollider2D myCollider;
     public Animator animator;
@@ -38,6 +39,7 @@ public class GoomechScript : MonoBehaviour
     public float groundCheckHeight = 1f;
     private bool approachingWall = false;
     private bool approachingEnemy = false;
+    private bool approachingSpike = false;
     private bool groundInFront = true;
 
     [Header("Statistics")]
@@ -67,6 +69,7 @@ public class GoomechScript : MonoBehaviour
         health = startingHealth;
         movementEnabled = true;
         groundLayer = LayerMask.GetMask("Ground", "Plank Ground", "Wall");
+        spikeLayer = LayerMask.GetMask("Spike");
         enemyLayer = LayerMask.GetMask("Enemy");
     }
 
@@ -79,9 +82,10 @@ public class GoomechScript : MonoBehaviour
             return;
         }
         approachingWall = Physics2D.Raycast(transform.position, facingRight ? Vector2.right : Vector2.left, horizontalCheckLength, groundLayer);
+        approachingSpike = Physics2D.Raycast(transform.position, facingRight ? Vector2.right : Vector2.left, horizontalCheckLength *1.65f, spikeLayer);
         approachingEnemy = Physics2D.Raycast(transform.position + Vector3.right * (horizontalCheckLength - .01f) * (facingRight ? 1 : -1), facingRight ? Vector2.right : Vector2.left, horizontalCheckLength, enemyLayer);
         groundInFront = Physics2D.Raycast(transform.position + Vector3.right * horizontalCheckLength * (facingRight ? 1 : -1), Vector2.down, groundCheckHeight, groundLayer);
-        if (approachingWall || approachingEnemy || !groundInFront)
+        if (approachingWall || approachingEnemy || approachingSpike || !groundInFront)
         {
             Flip();
         }
