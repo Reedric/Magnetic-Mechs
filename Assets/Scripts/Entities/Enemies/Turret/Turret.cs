44ea38a28e27b458296d0a777c23e752420303a0
Rishi Aitha <rishi.aitha@gmail.com>
2025-10-01 19:25:34 -0400

    simplified turret building

--- Assets/Scripts/Entities/Enemies/Turret/Turret.cs
+++ Assets/Scripts/Entities/Enemies/Turret/Turret.cs
@@ -6,15 +6,7 @@ public class Turret : BulletSpawnerParent
     // Store this script in the turret chamber
     // Inherits from the bullet spawner parent script, similar to BulletSpawnerScript
 
-    [Header("Object Components")]
-
-    [SerializeField] private GameObject turretHolder;
-    [SerializeField] private GameObject turretChamber;
-
     [Header("Enemy Specifications")]
-
-    [Tooltip("Angle of turret's base")]
-    [SerializeField] private float baseAngle;
 
     protected float shootingAngle = 0f;
 
@@ -30,14 +22,13 @@ public class Turret : BulletSpawnerParent
     public void SetUpTurret()
     {
         // Set the base and shooting angles based on specified values
-        turretHolder.transform.eulerAngles = new Vector3(0f, 0f, baseAngle);
         transform.eulerAngles = new Vector3(0f, 0f, shootingAngle);
 
         // increase bullet force to account for large missile mass for letting players walk on bullet
         bulletForce = missileForce * 10000;
         timer = timeBetweenShots;
         audioBox = gameObject.GetComponent<AudioSource>();
-        parentObject = turretChamber;
+        parentObject = gameObject;
         SetUpGameObjects();
     }
 
